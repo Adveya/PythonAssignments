2d85782c86752888dc7d6772517d77ff1753743f
Adveya <74756504+Adveya@users.noreply.github.com>
2020-12-07 02:33:29 +0530

    calendar with input('Enter a month, year')

--- Day1/Calendar.py
+++ Day1/Calendar.py
@@ -1,1 +1,60 @@
+mm=int(input("Enter the month"))
+yy=int(input("Enter the year"))
+
+month={1:'January', 2:'February',3:'March', 4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
+
+day=(yy-1)%400
+day=day%7
+day=(day//100)*5+((day%100)-(day%100)//4)+((day%100)//4)*2
+
+nonLY=[31,28,31,30,31,30,31,31,30,31,30,31]
+LY=[31,29,31,30,31,30,31,31,30,31,30,31]
+L=0
+
+if yy%4==0:
+    for i in range(mm-1):
+        L+=LY[i]
+
+else:
+    for i in range(mm-1):
+        L+=nonLY[i]
+
+day+= L%7
+day= day%7
+space=' '
+space=space.rjust(2,' ')
+
+print(month[mm],yy)
+print('Sun','Mon','Tue','Wed','Thu','Fri','Sat')
+
+if mm==4 or mm==6 or mm==9 or mm==11:
+    for i in range(day+31):
+        if i<=day:
+            print(space,end=' ')
+        else:
+            print("{:02d}".format(i-day), end=' ')
+            
+            if (i+1)%7==0:
+                print()
+elif mm==2:
+    if yy%4==0:
+        s=30
+    else:
+        s=29
+    for i in range(s+day):
+        if i<=day:
+            print(space, end=' ')
+        else:
+             print("{:02d}".format(i-day), end=' ')
+             if (i+1)%7==0:
+                    print()
+else:
+    for i in range(day+32):
+        if i<=day:
+            print(space, end=' ')
+        else:
+            print("{:02d}".format(i-day), end=' ')
+            if (i+1)%7==0:
+                    print()
+
 
